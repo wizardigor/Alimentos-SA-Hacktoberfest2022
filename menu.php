472af569688f2453a225557cf578b3f2c9f00d38
Igor Gomes <igorgomesads@gmail.com>
2022-09-30 19:44:10 -0300

    add
    base da aplicação

---
+++ menu.php
@@ -0,0 +1,13 @@
+<?php
+class Menu{
+    function getNavbar(){
+        return "
+        <nav class='navbar navbar-light bg-light'>
+        <a class='navbar-brand' href='#'>
+            <img src='img/logo.png' width='30' height='30' class='d-inline-block align-top' alt=''>
+            Alimentos S.A.
+        </a>
+        </nav>
+        ";
+    }
+}
